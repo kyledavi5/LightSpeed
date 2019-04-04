d45e1d3d62c770eca16bb441a824203aa33e6a33
Kyle Davis <kyle@daviscloud.net>
2019-04-03 21:55:47 -0400

    Add project files.

---
+++ Src/LightSpeed/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using LightSpeed.Views;
+using System.Windows;
+
+namespace LightSpeed
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        { 
+
+        }
+    }
+}
