2551055617dc2134e4e691c41ec84356856dcee1
Kyle Davis <kyle@daviscloud.net>
2019-04-04 12:29:48 -0400

    Added LightSpeed.Core project

---
+++ Src/LightSpeed.Core/CoreModule.cs
@@ -0,0 +1,20 @@
+﻿using LightSpeed.Core.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace LightSpeed.Core
+{
+    public class CoreModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
